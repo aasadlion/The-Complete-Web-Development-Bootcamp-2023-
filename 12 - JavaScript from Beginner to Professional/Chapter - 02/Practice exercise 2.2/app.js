f68874e96a37ec861571246c8122e5a9cc307018
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2023-10-18 00:51:08 +0500

    javascript Practice exercise 2.2 solved

---
+++ 12 - JavaScript from Beginner to Professional/Chapter - 02/Practice exercise 2.2/app.js
@@ -0,0 +1,10 @@
+/* Practice exercise 2.2
+Create a variable for your name, another one for your age, and another one for
+whether you can code JavaScript or not.
+Log to the console the following sentence, where name, age and true/false are
+  */
+
+var myName="Muhammad Asad iqbal";
+var myAge=22;
+var javascriptCdoe=true;
+console.log("Hello, my name is "+myName+ " "+"I am "+ myAge +" years old" +" "+ "and I can code JavaScript:"+" "+javascriptCdoe );
