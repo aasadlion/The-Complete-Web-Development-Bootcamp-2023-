451b4a896ec31878513213911f7af455321dbcbd
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2023-10-17 23:26:22 +0500

    Chapter 01 Getting Started with JavaScript Project

---
+++ 12 - JavaScript from Beginner to Professional/Chapter - 01/Chapter project/app.js
@@ -0,0 +1,9 @@
+console.log("Muhammad Asad Iqbal"); // 
+
+/* Nicely done! I'm have made a start with JavaScript! In this chapter, we have
+discussed a lot of context, which you will need to know before starting to code
+JavaScript. We saw that we can use JavaScript for many purposes, and one of the
+most popular use cases is the web. Browsers can work with JavaScript because they
+have a special part, called an interpreter, that can process JavaScript. We saw that
+we have multiple options for writing JavaScript on our computer. We will need an
+IDE, a program that we can use to write and run our code */
