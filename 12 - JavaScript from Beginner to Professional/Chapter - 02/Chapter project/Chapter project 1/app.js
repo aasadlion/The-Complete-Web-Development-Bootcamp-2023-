a8c72684c41679092cb8e940b041096c377754e3
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2023-10-18 01:42:50 +0500

    javascript Chapter 2 project 1 solved

---
+++ 12 - JavaScript from Beginner to Professional/Chapter - 02/Chapter project/Chapter project 1/app.js
@@ -0,0 +1,10 @@
+/* Chapter project
+Miles-to-kilometers converter
+Create a variable that contains a value in miles, convert it to kilometers, and log the
+value in kilometers in the following format: 
+ */
+
+var distance=209;
+var kilometers;
+kilometers=distance*1.60934;
+console.log(`The distance of ${distance} miles is equal to ${kilometers} kms`);
