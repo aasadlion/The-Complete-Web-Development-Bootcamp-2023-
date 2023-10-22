b70ee793f928f59e8c24ba872da97a1c3b22c740
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2023-10-22 12:41:51 +0500

    Practice exercise 3.3

---
+++ 12 - JavaScript from Beginner to Professional/Chapter - 03/Practice exercise 3.3/app.mjs
@@ -0,0 +1,9 @@
+// Practice exercise 3.3
+// 1. Create an array containing three values: 1, 2, and 3.
+// 2. Nest the original array into a new array three times.
+// 3. Output the value 2 from one of the arrays into the console
+
+
+let arra=[1,2,3];
+let newArray=[arra,arra,arra];
+console.log(newArray[1])
