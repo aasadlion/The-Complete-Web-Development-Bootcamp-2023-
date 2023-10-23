5454a500547fb68fa62e408bdb531768df8503a2
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2023-10-23 12:04:33 +0500

    Practice exercise 4.4

---
+++ 12 - JavaScript from Beginner to Professional/Chapter - 04/Practice exercise 4.4/app.mjs
@@ -0,0 +1,19 @@
+let grade = "B";
+
+
+switch (grade) {
+    case "A":
+        console.log("Nice!")
+        break;
+    case "B":
+        console.log("Good!")
+        break;
+    case "C":
+        console.log("Good!")
+        break;
+    case "D":
+        console.log("You've failed!")
+        break;
+    default:
+        console.log("I don't know this grade.");
+}
