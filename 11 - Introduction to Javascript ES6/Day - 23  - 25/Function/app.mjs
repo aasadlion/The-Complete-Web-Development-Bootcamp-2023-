0b3e09bef89e309479015d3d3c2d48f699a85122
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2023-10-25 18:22:15 +0500

    Day 23 javascrip funcations

---
+++ 11 - Introduction to Javascript ES6/Day - 23  - 25/Function/app.mjs
@@ -0,0 +1,10 @@
+function calDaysWeeksYears(age) {
+    
+let months=age *12;
+let weeks=52*age;
+let days=age*365;
+
+console.log(`Your age is ${age} and you spend ${days} Days, ${weeks} Weeks, ${months} Months`)
+}
+calDaysWeeksYears(23);
+
