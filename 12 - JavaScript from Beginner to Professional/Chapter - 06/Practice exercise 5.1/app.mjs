f6e45fa9266c934034fad330455ea2a7d02bf67f
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2023-10-29 21:29:28 +0500

    Practice exercise 6.1

---
+++ 12 - JavaScript from Beginner to Professional/Chapter - 06/Practice exercise 5.1/app.mjs
@@ -0,0 +1,21 @@
+// Practice exercise 6.1
+// See if you can write a function for yourself. We want to write a function that adds 
+// two numbers.
+// 1. Create a function that takes two parameters, adds the parameters together, 
+// and returns the result.
+// 2. Set up two different variables with two different values.
+// 3. Use your function on the two variables, and output the result using 
+// console.log.
+// 4. Create a second call to the function using two more numbers as arguments 
+// sent to the function
+
+function add(num1,num2) {
+    
+    
+    let add=num1+num2;
+    console.log(add);
+    
+}
+
+add(10,10);
+add(100,10);
