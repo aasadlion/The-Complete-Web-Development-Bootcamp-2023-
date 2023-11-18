f62c3f80ce2802a2e092b86a56f7d71c8ce3e33c
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2023-11-18 20:26:10 +0500

    day 01 Started learn Introduction To TypeScript Programming

---
+++ 13 - TypeScript/02 Variables In TypeScript/app.js
@@ -0,0 +1,7 @@
+"use strict";
+var firstName = "Muhammad";
+var lastName = "Asad";
+console.log(firstName, lastName);
+let num1 = 20;
+let num2 = 49;
+console.log(num1 + num2);
---
+++ 13 - TypeScript/02 Variables In TypeScript/app.ts
@@ -0,0 +1,6 @@
+ var firstName:string ="Muhammad";
+ var lastName:string ="Asad";
+ console.log(firstName,lastName)
+let num1:number=20;
+let num2:number=49;
+console.log(num1+num2);
